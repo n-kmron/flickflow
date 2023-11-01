c94963f24e91a184861070bb41f1cc8ab640944a
n-kmron <cameronnoupoue@gmail.com>
2023-11-01 22:36:34 +0100

    release 1 : center image on the screen

--- app/src/main/java/g58008/mobg5/ui/Esi.kt
+++ app/src/main/java/g58008/mobg5/ui/Esi.kt
@@ -2,7 +2,10 @@ package g58008.mobg5.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,7 +16,10 @@ import g58008.mobg5.ui.theme.AppTheme
 fun EsiScreen() {
     val image = painterResource(R.drawable.esi_logo)
 
-    Box {
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ){
         Image(
             painter = image,
             contentDescription = "ESI LOGO",
